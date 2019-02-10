c5fdfe70a8df2dd1de8571c4bb30185aa931b861
christina <anastasiou@DESKTOP-KQ8JB1B.localdomain>
2019-02-10 13:35:25 +0100

    add CSS Module

--- src/Bakery.js
+++ src/Bakery.js
@@ -1,40 +1,38 @@
 import React, { Component } from 'react';
-import {
-  Container,
-  Row,
-  Col
-} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './Bakery.module.css';
 
 class Bakery extends Component {
   render() {
     return (
-        <Container className="bakery">
+        <Container className={styles.wrapper}>
           <Row>
             <Col xs={{ size: 3, offset: 9 }}>
-              <div className="sun"></div>
+              <div className={styles.sun}></div>
             </Col>
           </Row>
           <Row>
             <Col>
-              <div className="triangle"></div>
-              <div className="title">
+              <div className={styles.triangle}></div>
+              <div className={styles.headline}>
                 <h4>COOKIE BAKERY</h4>
               </div>
             </Col>
           </Row>
-          <Row className="square">
+          <Row className={styles.square}>
             <Col xs="4">
-              <div className="cookie"></div>
+              <div className={styles.cookie}></div>
             </Col>
             <Col xs="4">
-              <div className="door">
-                <div className="handle"></div>
+              <div className={styles.door}>
+                <div className={styles.handle}></div>
               </div>
             </Col>
             <Col xs="4">
-              <div className="cookie"></div>
-              <div className="mailbox">
-                <div className="hole"></div>
+              <div className={styles.cookie}></div>
+              <div className={styles.mailbox}>
+                <div className={styles.hole}></div>
                 <h5>Mailbox</h5>
               </div>
             </Col>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bakery from './Bakery';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Bakery.css';
 
-ReactDOM.render(<Bakery />, document.getElementById('root'));
+ReactDOM.render(
+    <Bakery />, 
+    document.getElementById('root')
+);
